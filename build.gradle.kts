c7df5a5fa440e88bf0148e5886e2477010bc05c0
bonaventuran <bonaventuran@previdom.previnet.it>
2022-08-26 14:07:12 +0200

    Dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+buildscript {
+	repositories {
+		google()
+		mavenCentral()
+		gradlePluginPortal()
+	}
+	dependencies {
+		classpath(libs.android.gradlePlugin)
+		classpath(libs.kotlin.gradlePlugin)
+
+		// https://stackoverflow.com/a/41958982/3853058
+		classpath(libs.dependencyVersion.gradlePlugin)
+
+		// NOTE: Do not place your application dependencies here; they belong in the individual module build.gradle files
+	}
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+	// FIXME
+	//id(BuildPlugins.gradleVersionPlugin) version libs.versions.dependencyVersion.get()
+	id(BuildPlugins.gradleVersionPlugin) version "0.42.0"
+}
+
+tasks.register("clean").configure {
+	delete("build")
+}
