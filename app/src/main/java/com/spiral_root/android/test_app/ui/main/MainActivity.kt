d00fe2fdcdf509eb3f50dc2d454422b2fa9fada9
bonaventuran <bonaventuran@previdom.previnet.it>
2022-08-29 10:41:20 +0200

    UI

--- app/src/main/java/com/spiral_root/android/test_app/ui/main/MainActivity.kt
+++ app/src/main/java/com/spiral_root/android/test_app/ui/main/MainActivity.kt
@@ -14,8 +14,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.spiral_root.android.test_app.ui.theme.TestAppTheme
+import com.spiral_root.android.test_app.utils.collectAsStateLifecycleAware
 import dagger.hilt.android.AndroidEntryPoint
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -33,12 +37,16 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Home(name: String, modifier: Modifier = Modifier) {
+fun Home(
+	name: String,
+	onClick: () -> Unit, lastActionTime: LocalDateTime?,
+	modifier: Modifier = Modifier
+) {
 	ConstraintLayout(
 		modifier = modifier
 			.fillMaxWidth()
 	) {
-		val (titleRef, buttonRef) = createRefs()
+		val (titleRef, buttonRef, lastActionTimeRef) = createRefs()
 		Text(
 			modifier = Modifier
 				.constrainAs(titleRef) {
@@ -57,16 +65,40 @@ fun Home(name: String, modifier: Modifier = Modifier) {
 					end.linkTo(parent.end)
 					centerHorizontallyTo(parent)
 				},
-			onClick = { /*TODO*/ }) {
+			onClick = onClick
+		) {
 			Text(text = "Click me :)")
 		}
+		val lastActionValue = if (lastActionTime == null) "---" else DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(lastActionTime)
+		Text(
+			modifier = Modifier
+				.constrainAs(lastActionTimeRef) {
+					top.linkTo(buttonRef.bottom, margin = 5.dp)
+					start.linkTo(parent.start)
+					end.linkTo(parent.end)
+					centerHorizontallyTo(parent)
+				},
+			text = "Last action time: $lastActionValue"
+		)
 	}
+}
+
+@Composable
+fun Home(name: String, viewModel: MainViewModel = hiltViewModel()) {
+	val lastActionTime = viewModel.lastActionTime.collectAsStateLifecycleAware()
+	Home(
+		name = name,
+		onClick = { viewModel.doAction() }, lastActionTime = lastActionTime.value
+	)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
 	TestAppTheme {
-		Home("Android")
+		Home(
+			"Android",
+			{}, LocalDateTime.of(2022, 8, 29, 10, 30, 0, 0)
+		)
 	}
 }
