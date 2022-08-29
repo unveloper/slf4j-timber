2f67d5715213df300673162f52ca0dd4b5c952ca
bonaventuran <bonaventuran@previdom.previnet.it>
2022-08-29 09:52:23 +0200

    "data" and "domain" modules

--- app/src/main/java/com/spiral_root/android/test_app/MainActivity.kt
+++ app/src/main/java/com/spiral_root/android/test_app/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.spiral_root.android.test_app.ui.theme.TestAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
